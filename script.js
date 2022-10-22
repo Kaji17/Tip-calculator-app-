edf4bf31fa2f45c8c8e8cff6b3d3a48e4dc6571f
Ouattara Katina <ouattarapevirgile@gmail.com>
2022-10-22 03:50:24 +0000

    disabled or enabled button

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const resetbutton = document.getElementById("btnreset");
-const input = document.getElementById("inputbill");
+const input = document.getElementsByTagName('input');
+const form = document.getElementById('myform');
 
-// permi to disabled button reset if input bill empty
-input.addEventListener("keyup", (e)=>{
-   e.preventDefault();
-   const value = e.currentTarget.value;
-   if (value === '') {
-    resetbutton.setAttribute('disabled', 'disabled');
-   }else{
-    resetbutton.removeAttribute("disabled");
-    console.log('remove')
-   }
+// permi to disabled button reset if liste input bill empty
+for (let i = 0; i < input.length; i++) {
+    const element = input[i];
+    // permi to disabled button reset if input bill empty
+    element.addEventListener("keyup", (e) => {
+        e.preventDefault();
+        const value = e.currentTarget.value;
+        if (value === '') {
+            resetbutton.setAttribute('disabled', 'disabled');
+        } else {
+            resetbutton.removeAttribute("disabled");
+            console.log('remove')
+        }
 
-})
+    })
+}
+
+
+
+
+
+
