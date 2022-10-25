532a80e0966ee326b91b0a77a23ccdad01c01106
Ouattara Katina <ouattarapevirgile@gmail.com>
2022-10-25 13:36:37 +0000

    change color of button reset when enable

--- script.js
+++ script.js
@@ -20,18 +20,19 @@ for (let i = 0; i < input.length; i++) {
 }
 
 for (let i = 0; i < input.length; i++) {
-    const element = fo[i];
+    const element = input[i];
     // permit to disabled button reset if input bill empty
     element.addEventListener("keyup", (e) => {
         e.preventDefault();
         const value = e.currentTarget.value;
         if (value === '') {
+            document.getElementById("btnreset").style.backgroundColor="#0d686d";
             resetbutton.setAttribute('disabled', 'disabled');
         } else {
             resetbutton.removeAttribute("disabled");
-            console.log('remove')
             console.log('remove');
             document.getElementById("btnreset").style.backgroundColor="hsl(172, 67%, 45%)";
+        }
 
     })
 }
